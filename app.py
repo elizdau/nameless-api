a1471a49f18bed52dbaf91d64f23e293bace8b56
elizdau <elizdau@gmail.com>
2025-04-10 10:08:10 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
 @app.route("/hello", methods=["GET"])
 def hello():
-    return {"message": "Hello from Liz’s server!"}
+    return jsonify({"message": "Hello from Liz’s server!"})
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=10000)
